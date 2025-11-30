f8d2002bc89530bdc9954e1250d675552fce38ad
novelbytelabs <novelbytelabs@gmail.com>
2025-11-30 13:57:19 -0500

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json"
+  },
+  plugins: [
+    "@typescript-eslint"
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  rules: {
+    // Customize here
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }]
+  }
+};
---
+++ src/arqonbus/main.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    print("Hello from arqonbus!")
+
+
+if __name__ == "__main__":
+    main()
