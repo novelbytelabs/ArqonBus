6970f46b19de7ab13f5664bb567c99ff50179a45
novelbytelabs <novelbytelabs@gmail.com>
2025-12-12 10:08:43 -0500

    build: Add Rust debug compilation artifacts.

--- crates/proto/src/lib.rs
+++ crates/proto/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub mod v1 {
+    include!(concat!(env!("OUT_DIR"), "/arqon.v1.rs"));
 }
